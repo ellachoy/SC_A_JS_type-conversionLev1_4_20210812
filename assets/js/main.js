c78d5526b47e043ee24e0dd158899e437d51872e
Ella Choy <egal21de@gmail.com>
2021-08-12 16:56:14 +0200

    type-conversion

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+console.log("hi type-conversion")
+document.write("type-conversion")
+
+
+// Was sind die Datentypen dieser Werte? Überlege nochmal mit welcher Funktion Du Datentyp bestimmen kannst. 
+
+
+let name="John";
+console.log(typeof(name));
+
+const pi=3.14;
+console.log(typeof(pi));
+
+let was=NaN;
+console.log(typeof(was));
+
+let status=false;
+console.log(typeof(status));
+
+console.log(typeof([1,2,3,4]));
+
+console.log(typeof({name:'John', age:34}));
+
+console.log(typeof(function() {} )); //function
+
+
+console.log(typeof(null)); 
+
+console.log(typeof("")); //string
+
+console.log(typeof(3+2==5)); //boolean
+
+console.log(typeof(3 + "3")); //string
+
+
